461260f11d94766b72fb3fc965f969dca01dde16
Michael Haas <michael.haas@btelligent.com>
2022-11-11 14:52:03 +0100

    change name for stack test

--- code/cdk-workshop/tests/unit/test_simple_microservice_stack.py
+++ code/cdk-workshop/tests/unit/test_simple_microservice_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from cdk_workshop.cdk_workshop_stack import CdkWorkshopStack
+from cdk_workshop.simple_microservice_stack import SimpleMicroserviceStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in cdk_workshop/cdk_workshop_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = CdkWorkshopStack(app, "cdk-workshop")
+    stack = SimpleMicroserviceStack(app, "simple-microservice")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
